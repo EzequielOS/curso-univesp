09ee172b8ca7583d7dc928f83a71b6d39ad28711
Ezequiel Oliveira <ezequiel.santana@sankhya.com.br>
2018-09-27 17:16:43 -0300

    2nd commit: Execução e criação do HelloWorld, e .gitignore

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,8 @@
 public class HelloWorld {
 
-  public static void main (string[]args){
-  //prints "Hello, World" to the terminal window
-  System.out.printIn("Hello,World");
-  }
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World Men");
+    }
 
 }
