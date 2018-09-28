e0a92105ff0a66c1d2401a43f77ee37cbbb01c94
Ezequiel Oliveira <ezequiel.santana@sankhya.com.br>
2018-09-28 17:31:26 -0300

    5th commit: Aula 06/a Simplificando o Uso de variaveis

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,22 +1,24 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
+      int lateral = 10;
+      int cquarto = 7;
       int areaq; //área do quarto
       int areas; //área da sala
       int areat; //area tootal
 
         // Isso printa a mennsagem "Programa Para o Calculo da area da Casa"
         System.out.println("Programa Para o Calculo da area da Casa");
-        areas = 10*10;
+        areas = lateral*lateral;
         System.out.println("A area da sala equivale "+areas);
-        areaq = 7*5;
+        areaq = cquarto*(lateral/2);
         System.out.println("A area do quarto equivale "+areaq);
+        System.out.println("A area do banheiro equivale "+areaq);
         areat = areas + 2*areaq;
         System.out.println("A area total equivale: "+areat);
 
 
 
     }
-
 
 }
