891524fdec04b1d9d28bd8914346b4297f3c5e54
Ezequiel Oliveira <ezequiel.santana@sankhya.com.br>
2018-09-28 17:23:45 -0300

    4th commit: Aula 05 Criação de Variaveis (Usando exemplo UNIVESP-TV)

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,22 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
-        // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, World Men");
+      int areaq; //área do quarto
+      int areas; //área da sala
+      int areat; //area tootal
+
+        // Isso printa a mennsagem "Programa Para o Calculo da area da Casa"
+        System.out.println("Programa Para o Calculo da area da Casa");
+        areas = 10*10;
+        System.out.println("A area da sala equivale "+areas);
+        areaq = 7*5;
+        System.out.println("A area do quarto equivale "+areaq);
+        areat = areas + 2*areaq;
+        System.out.println("A area total equivale: "+areat);
+
+
+
     }
+
 
 }
