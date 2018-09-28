57da1cafa1c00a33915d0ff1ffb817bc7468b1d3
Ezequiel Oliveira <ezequiel.santana@sankhya.com.br>
2018-09-28 17:58:48 -0300

    6th commit: Aula 06/b Introducao da piscinadohello (Variaveis double)

---
+++ piscinadohello.java
@@ -0,0 +1,12 @@
+public class piscinadohello {
+
+    public static void main(String[] args) {
+      //raio da piscina
+      double raio = 2;
+      //area da piscina
+      double areap;
+
+      areap = Math.PI * Math.pow(raio,2);
+
+      System.out.println("Area da piscinadohello: "+areap);
+    }}
