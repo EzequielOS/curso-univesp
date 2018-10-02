a781334aa4c833fb97ae64260643afde75091be3
Ezequiel Oliveira <ezequiel.santana@sankhya.com.br>
2018-10-02 17:55:02 -0300

    9th commit: Aula 07/b Chamado Métodos em AreaCasa (Simplificou o código)

--- AreaCasa.java
+++ AreaCasa.java
@@ -1,12 +1,11 @@
 public class AreaCasa {
 
-    public static void main(String[] args) {
+   static void Cabana() {
       float lateral = 11;
       float cquarto = 7;
       float areaq; //área do quarto
       float areas; //área da sala
       float areat; //area tootal
-
         // Isso printa a mennsagem "Programa Para o Calculo da area da Casa"
         System.out.println("Programa Para o Calculo da area da Casa");
         areas = lateral*lateral;
@@ -16,22 +15,30 @@ public class AreaCasa {
         System.out.println("A area do banheiro equivale "+areaq);
         areat = areas + 2*areaq;
         System.out.println("A area total equivale: "+areat);
-
+}
 //===================================================
 //========== Aqui Começa ===============
 //====== A Area da piscinadohello =======
 //====== E termina ==========
 //========= A Area da CABANA =====================
 //=====================================================
-
+ static double Piscina () {
 //raio da piscina
 double raio = 2;
+  return (Math.PI * Math.pow(raio,2));
+}
+//=================
+//===== A execuçao do Programa
+// ===== começa Aqui
+//========
+public static void main(String[] args) {
 //area da piscina
 double areap;
+Cabana();
+areap = Piscina();
 
-areap = Math.PI * Math.pow(raio,2);
+System.out.println("Area da Piscina: "+areap);
 
-System.out.println("Area da piscinadohello: "+areap);
-    }
+}
 
 }
