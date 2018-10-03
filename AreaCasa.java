0c1a5c49c7e452de6aba06fbd8a92f73c1e12c0e
Ezequiel Oliveira <ezequiel.santana@sankhya.com.br>
2018-10-03 17:11:14 -0300

    11th commit: Aula 08 Atributos (Adição de uma variavel Global)

--- AreaCasa.java
+++ AreaCasa.java
@@ -1,8 +1,10 @@
 public class AreaCasa {
+//isso é um atributo=========
+//Variavel Global ou seja pode ser usada em todos os metodos
+static double valorM2 = 1500;
+//==== !!!!NÃO FAÇA TODAS AS VARIAVEIS GLOBAIS !!!!!!! ========
 
-   static void Cabana() {
-      float lateral = 11;
-      float cquarto = 7;
+   static void Cabana(float lateral, float cquarto) {
       float areaq; //área do quarto
       float areas; //área da sala
       float areat; //area tootal
@@ -16,13 +18,19 @@ public class AreaCasa {
         areat = areas + 2*areaq;
         System.out.println("A area total equivale: "+areat);
 }
+//
+static double valor(double area){
+valorM2 = 5;
+return (valorM2*area);
+}
+
 //===================================================
 //========== Aqui Começa ===============
-//====== A Area da piscinadohello =======
+//====== A Area da Piscina =======
 //====== E termina ==========
 //========= A Area da CABANA =====================
 //=====================================================
- static double Piscina(double raio){
+ static double Piscina(double raio){ //variavel declarada no parametro
   return (Math.PI * Math.pow(raio,2));
 }
 //=================
@@ -32,8 +40,8 @@ public class AreaCasa {
 public static void main(String[] args) {
 //area da piscina
 double areap;
-Cabana();
-areap = Piscina(2);
+Cabana(11,7);
+areap = Piscina(2); //valor declarado no parametro para variavel raio =2
 
 System.out.println("Area da Piscina: "+areap);
 
