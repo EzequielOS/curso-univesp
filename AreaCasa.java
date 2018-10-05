e428167985ade80af0a12fee36bcce44e1e42531
Ezequiel Oliveira <ezequiel.santana@sankhya.com.br>
2018-10-05 15:04:54 -0300

    13th commit: Aula 10/a Condicionais Aninhados (print != println)

--- AreaCasa.java
+++ AreaCasa.java
@@ -11,18 +11,25 @@ return (-1);
 }
 
    static void Cabana(float lateral, float cquarto) {
-      float areaq; //área do quarto
-      float areas; //área da sala
-      float areat; //area tootal
+      float areaq; float areas; float areat;
+    //área do quarto //área da sala //area tootal //respectivamente
+
+//Aninhamento de CONDICIONAIS
+  if (lateral<0) System.out.println("Erro:lateral da sala menor que 0");
+  else {
+        if (cquarto<0) System.out.println("Erro:lateral da quarto menor que 0");
+        else{
         // Isso printa a mennsagem "Programa Para o Calculo da area da Casa"
         System.out.println("Programa Para o Calculo da area da Casa");
         areas = lateral*lateral;
-        System.out.println("A area da sala equivale "+areas);
+        System.out.println("A area da sala equivale: "+areas);
         areaq = cquarto*(lateral/2);
-        System.out.println("A area do quarto equivale "+areaq);
-        System.out.println("A area do banheiro equivale "+areaq);
+        System.out.println("A area do quarto equivale: "+areaq);
+        System.out.println("A area do banheiro equivale: "+areaq);
         areat = areas + 2*areaq;
         System.out.println("A area total equivale: "+areat);
+    }
+  }
 }
 
 //===================================================
@@ -32,7 +39,8 @@ return (-1);
 //========= A Area da CABANA =====================
 //=====================================================
  static double Piscina(double raio){ //variavel declarada no parametro
-  return (Math.PI * Math.pow(raio,2));
+   if (raio >= 0)  return Math.PI * Math.pow(raio,2);
+   else return(-1);
 }
 //=================
 //===== A execuçao do Programa
@@ -51,8 +59,9 @@ double preco;
 boolean valorOK = false; //sempre true ou false
 preco = valor(20);
 valorOK = preco >= 0;
-if(valorOK)System.out.println("Valor para construir:"+preco);
-else System.out.println("Valor para construir: NAO PODE SER NEGATIVO!!!");
+System.out.print("Valor para construir "); //o print difere != do println, pois nao salta linha ao final
+if(valorOK)System.out.println("equivalente:"+preco);
+else System.out.println("nao foi obtido: AREA NEGATIVA!!!");
 
 }
 
