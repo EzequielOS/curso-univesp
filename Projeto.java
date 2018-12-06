57bb91d5c8fefe7c7b6013326a8bb1060c308941
Ezequiel Oliveira <ezequiel.santana@sankhya.com.br>
2018-12-06 15:34:08 -0200

    Cria Residencia.java para caculo de multiplas casas em um condominio

--- Projeto.java
+++ Projeto.java
@@ -1,7 +1,7 @@
 /*
 Autor: Ezequiel de Oliveira Sant'Ana
 
-Criado em: 04/05/2018
+Criado em: 04/12/2018
 
 O objetivo de criação do documento é o aprendizado e a pratica na linguagem JAVA.
 A real função do código é executar dois codigos contidos no repositório.
@@ -13,15 +13,42 @@ Link de acesso: https://www.youtube.com/playlist?list=PLxI8Can9yAHfK6wdaMUO74lmo
 */
 
 public class Projeto {
-    static double area (AreaCasa casa, AreaPiscina piscina){
-        return(casa.Cabana() + piscina.Piscina());
+    Residencia[] condominio;
+    int ultimo = -1; //ultimo alocado
+
+    boolean adicionaRes(Residencia r){
+        if(this.ultimo < this.condominio.length - 1){
+            ultimo++;
+            this.condominio[ultimo] = r;
+            return(true);
+        }
+        return(false);
+    }
+
+    Projeto(int tam){
+        condominio = new Residencia[tam];
     }
 
     public static void main (String[] args){
-        AreaCasa casa1 = new AreaCasa(10,5);
-        AreaCasa casa2 = new AreaCasa(20,5);
-        AreaPiscina piscina1 = new AreaPiscina(10);
+        Projeto proj = new Projeto(3);
 
-        System.out.println(area(casa1, piscina1));
+        AreaCasa c = new AreaCasa(10,5);
+        AreaPiscina p = new AreaPiscina(5);
+        Residencia r = new Residencia(c,p);
+        proj.adicionaRes(r);
+
+        c = new AreaCasa(12,7);
+        p = new AreaPiscina(6);
+        r = new Residencia(c, p);
+        proj.adicionaRes(r);
+
+        c = new AreaCasa(10,6);
+        p = new AreaPiscina(3.5);
+        r = new Residencia(c, p);
+        proj.adicionaRes(r);
+
+        System.out.println("Tamanho da Casa1 (10x5) Piscina (5): " + proj.condominio[0].casa.Cabana() + "m2");
+        System.out.println("Tamanho da Casa2 (12x7) Piscina (6): " + proj.condominio[1].casa.Cabana() + "m2");
+        System.out.println("Tamanho da Casa3 (10x6) Piscina (3.5): " + proj.condominio[2].casa.Cabana() + "m2");
     }
 }
---
+++ Residencia.java
@@ -0,0 +1,23 @@
+/*
+Autor: Ezequiel de Oliveira Sant'Ana
+
+Criado em: 06/12/2018
+
+O objetivo de criação do documento é o aprendizado e a pratica na linguagem JAVA.
+A real função do código é calcular a area de um determinado espaço e o preço para sua construção, respectivamente.
+
+"Baseado no curso  Programação de Computadores da  Univesp - Universidade Virtual do Estado de São Paulo.
+Professores responsáveis: Luciano Digiampietri e Norton Trevisan Roman
+Link de acesso: https://www.youtube.com/playlist?list=PLxI8Can9yAHfK6wdaMUO74lmotAP7J7bi "
+*/
+
+class Residencia{
+    AreaCasa casa;
+    AreaPiscina piscina;
+
+    Residencia(AreaCasa casa, AreaPiscina piscina){
+        this.casa = casa;
+        this.piscina = piscina;
+    }
+
+}
